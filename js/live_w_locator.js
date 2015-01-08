d56d88238816b2020032474acdb8d4d8ccf165af
nandoAC <f.alvan-corcuera-09@student.lboro.ac.uk>
2015-01-08 00:29:04 +0000

    Barcode pages

--- js/live_w_locator.js
+++ js/live_w_locator.js
@@ -30,15 +30,9 @@ $(function() {
     App.init();
 
     Quagga.onDetected(function(result) {
-        if (App.lastResult !== result) {
-            App.lastResult = result;
-            var $node = null, canvas = Quagga.canvas.dom.image;
-
-            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-            $node.find("img").attr("src", canvas.toDataURL());
-            $node.find("h4.code").html(result);
-            $("#result_strip ul.thumbnails").prepend($node);
-        }
+        
+            window.location.href='barcodeProd.html';
+        
     });
 
 });
